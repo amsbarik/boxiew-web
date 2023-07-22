7ee7b476bf8c09118220af07bf6622b392f61de5
amsbarik <amsbarik24@gmail.com>
2023-07-22 22:14:14 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
-function add_listing(){
-    document.getElementById("add-listing").src = "./image/icon/add-hover.png";
+// home page search box js
+let popup = document.getElementById("search-box");
+let hideBtn = document.getElementById("show-modal");
+let dirBtn = document.getElementById("listing-menu");
+
+function openPopup(){
+    popup.classList.add("active");
+    hideBtn.classList.add("disable");
+    dirBtn.classList.add("disable");
+    
 }
+
+function closePopup(){
+    popup.classList.remove("active");
+    hideBtn.classList.remove("disable");
+    dirBtn.classList.remove("disable");
+
+}
+
+
+// single products page js
+    let ProductImg = document.getElementById("product-img");
+    let SmallImg = document.getElementsByClassName("img-thum");
+
+    SmallImg[0].onclick = function(){
+        ProductImg.src = SmallImg[0].src;
+    }
+    
+    SmallImg[1].onclick = function(){
+        ProductImg.src = SmallImg[1].src;
+    }
+
+    SmallImg[2].onclick = function(){
+        ProductImg.src = SmallImg[2].src;
+    }
+
+    SmallImg[3].onclick = function(){
+        ProductImg.src = SmallImg[3].src;
+    }
+
+    SmallImg[4].onclick = function(){
+        ProductImg.src = SmallImg[4].src;
+    }
